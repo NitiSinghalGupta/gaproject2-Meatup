e5cd92fc9ecf815ca3ec04a40b6bc9a6b03cdb70
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-09-28 19:14:11 -0700

    add User to admin.py

--- meatup/admin.py
+++ meatup/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Profile
 from .models import Event
+from .models import User
+
 
 admin.site.register(Profile)
 admin.site.register(Event)
-
+admin.site.register(User)
