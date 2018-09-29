0917f93bf6df7c8f94840e3c6fc04efeb065a008
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-09-28 18:28:31 -0700

    remove django_extensions dep

--- meatup_django/settings.py
+++ meatup_django/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'meatup',
-    'django_extensions',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
